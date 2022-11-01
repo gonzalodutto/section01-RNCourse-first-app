16f02d7aba23952cdf76e86ebe6e7a200539c1fa
gonzalodutto <gonzalodutto@gmail.com>
2022-11-01 15:00:56 +0100

    15. Styling React Native Apps

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Gonza, como estas?</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.textStyling}> Otro texto por aqui</Text>
+      </View>
+      <Text style={styles.textStyling}>Hola Gonza, como estas?</Text>
+      <Button title="Tocame" />
     </View>
   );
 }
@@ -16,5 +18,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textStyling: {
+    margin: 16,
+    borderWidth: 1,
+    borderColor: "blue",
+    padding: 10,
+    borderRadius: 5,
   },
 });
